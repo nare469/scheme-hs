da98139559be4c45cac642f2e065129ab8362fc2
Narendran Srinivasan <nare469@gmail.com>
2016-06-17 00:05:27 -0400

    Made data types

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,14 +1,44 @@
 module Parser where
 
+import Control.Monad
 import Text.ParserCombinators.Parsec
 
-symbol :: Parser Char
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-ignoreSpaces :: Parser ()
-ignoreSpaces = skipMany space
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> symbol <|> digit)
+    let atom = first:rest
+    return $ case atom of
+                "#t" -> Bool True
+                "#f" -> Bool False
+                _ -> Atom atom
+
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+          <|> parseString
+          <|> parseNumber
 
 readExpr :: String -> String
-readExpr input = case parse (ignoreSpaces >> symbol) "Symbol" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "It's a match!"
